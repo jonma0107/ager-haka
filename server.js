3cf4b0238e7ff97a7083bb5a156376a9aeb9623c
Jonathan Mauricio Meza Bastidas <mbjonathan@misena.edu.co>
2021-12-13 00:22:01 -0500

    initial commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/ager-haka/'));
 
-app.get('/*', (req, res) =>
+app.get('*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/ager-haka/'}),
 );
 
