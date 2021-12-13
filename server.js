9fb1cd46611549c7036ec84cb2347f069b31d064
Jonathan Mauricio Meza Bastidas <mbjonathan@misena.edu.co>
2021-12-12 23:35:20 -0500

    initial commit

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/ager-haka'));
+app.use(express.static(__dirname + '/front-end/dist/ager-haka'));
 
 // Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/ager-haka/index.html'));
+app.get('*', (req, res) {
+  res.sendFile('./front-end/dist/ager-haka/index.html'));
 });
 
 // default Heroku port
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 8080);
