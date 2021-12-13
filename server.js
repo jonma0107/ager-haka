ef32524c49c6fed0f7ba1ba565a673dd98bbba5d
Jonathan Mauricio Meza Bastidas <mbjonathan@misena.edu.co>
2021-12-13 00:07:23 -0500

    initial commit

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const path = require('path');
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-// Serve static files
-app.use(express.static(${__dirname} front-end/dist/ager-haka));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/ager-haka'));
 
-// Send all requests to index.html
-app.get('*', (req, res)  => {
-  res.sendFile('./front-end/dist/ager-haka/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/ager-haka/'}),
+);
 
-// default Heroku port
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
