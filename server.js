0ec6af72532eaed5c43d28aad665477073aa6ff2
Jonathan Mauricio Meza Bastidas <mbjonathan@misena.edu.co>
2021-12-13 00:42:32 -0500

    initial commit

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const path = require('path');
 const app = express();
  
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/ager-haka'));
+app.use(express.static(${__dirname}/front-end/dist/ager-haka));
  
-app.get('/*', function(req,res) {
- 
-res.sendFile(path.join(__dirname+'/ager-haka/index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
 });
  
 // Start the app by listening on the default Heroku port
